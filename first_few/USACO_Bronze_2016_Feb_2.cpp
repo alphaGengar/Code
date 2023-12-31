7bd17f052cb3b1d7f114c39836e7b5c1aa8e3b9c
alphaGengar <150190618+alphaGengar@users.noreply.github.com>
2023-12-30 16:18:39 -0800

    Add files via upload

--- first_few/USACO_Bronze_2016_Feb_2.cpp
+++ first_few/USACO_Bronze_2016_Feb_2.cpp
@@ -2,23 +2,34 @@
 using namespace std;
 
 int main() {
-    freopen("cbarn.in", "r", stdin);
-    freopen("cbarn.out", "w", stdout);
+    freopen("pails.in", "r", stdin);
+    freopen("pails.out", "w", stdout);
+    int b1, b2, m; cin >> b1 >> b2 >> m;
+    int answer = 0;
+    if(m%b2 == 0 || m%b1 ==0){
+        cout << m;
+    } else {
+        int small = min(b1, b2);
+        int large = max(b1, b2);
+        int max1 = m / small;
+        int min2 = m / large;
+        answer = max(small*max1, large*min2);
+        
+        vector<int> counter(max1);
+        for(int i = min2; i <= max1; i++){
+            for(int j = 0; j < i; j++){counter[j] = small;}
+            for(int k = 0; k < i; k++){
+                counter[k] = large;
+                int count = 0;
+                for(auto& l : counter){
+                    count += l;
+                }
 
-    int n; cin >> n;
-    vector<int> rooms(n);
-    for(int i = 0; i < n; i++){cin >> rooms[i];}
-    
-    int record = -1;
-
-    for(int i = 0; i < n; i++){
-        int pre = 0;
-        for(int j = i+1; j < i+n; j++){
-            pre = pre + (j-i)*rooms[j%n];
+                if(count <= m && count > answer){
+                    answer = count;
+                }
+            }    
         }
-        if(record==-1 || pre < record){
-            record = pre;
-        }
+    cout << answer;
     }
-    cout << record;
 }
